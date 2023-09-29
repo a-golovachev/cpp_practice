b22c9339ece641f34fb77bf1dee83e143cdf84db
a-golovachev <alexkoel99@gmail.com>
2023-09-29 22:51:42 +0300

    Start big integer work

---
+++ BigInteger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aleksandr Golovachev on 29.09.2023.
+//
+
+#include "BigInteger.h"
---
+++ BigInteger.h
@@ -0,0 +1,8 @@
+//
+// Created by Aleksandr Golovachev on 29.09.2023.
+//
+
+#ifndef CPP_PRACTICE_BIGINTEGER_H
+#define CPP_PRACTICE_BIGINTEGER_H
+
+#endif //CPP_PRACTICE_BIGINTEGER_H
